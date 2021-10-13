2e771748e9c2c15fe10ed4cf8918852e1a24f6fe
Matheus Manhães <momanhaes@gmail.com>
2021-10-12 22:32:20 -0300

    fix: content

--- src/app/pages/home/home.interface.ts
+++ src/app/pages/home/home.interface.ts
@@ -78,7 +78,7 @@ export const CARD_CONTENT_CAMINHO_ADVOGADO = [
     target: Target.CONTATOS,
   },
   {
-    title: 'JUSRISPRUDÊNCIA',
+    title: 'JURISPRUDÊNCIA',
     subtitle: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
     when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
